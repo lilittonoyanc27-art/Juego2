39d5ade2107adaef6fe48c0c2d1a63c582ef5015
lilittonoyanc27-art <lilit.tonoyanc27@gmail.com>
2026-03-27 21:01:46 +0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import GorGayanePluralBattle from './GorGayanePluralBattle';
+
+export default function App() {
+  return (
+    <div className="relative min-h-screen">
+      <GorGayanePluralBattle />
+    </div>
+  );
+}
